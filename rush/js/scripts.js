6ab4db2f8df3519615f82b10e0d87b746489399f
Francisco Moran <fmoran@c1r10s4.42madrid.com>
2024-04-05 14:13:32 +0200

    rush

--- rush/js/scripts.js
+++ rush/js/scripts.js
@@ -37,18 +37,3 @@ window.addEventListener('DOMContentLoaded', event => {
             rootMargin: '0px 0px -40%',
         });
     };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
-
-});
